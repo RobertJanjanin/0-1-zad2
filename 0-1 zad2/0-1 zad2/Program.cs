882b180b3b1f79ea990ca26a56e749093c47cf02
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-12-04 15:35:15 +0100

    Add files via upload

---
+++ 0-1 zad2/0-1 zad2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_1_zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sekunde;
+            int minute;
+            int sekundi;
+
+
+            Console.WriteLine("Upisi broj sekundi: ");
+
+            sekunde = Convert.ToInt32(Console.ReadLine());
+
+            minute = sekunde / 60;
+
+            sekundi = sekunde % 60;
+
+            Console.WriteLine(+minute+" minute i  "+ sekundi+"sekundi");
+
+            Console.ReadKey();
+        }
+    }
+}
